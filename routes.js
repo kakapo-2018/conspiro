7b6af4b2199925ac407a33a9baf41ef422fa2568
rob-bee-neilson <robbie.neilson@gmail.com>
2018-08-17 09:40:49 +1200

    fix up

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let	server = express();
 let conspiracyData = require('./conspiracy.json');
-let nounData = require('./nouns.json');
-let verbData = require('./verbs.json');
+//let nounData = require('./nouns.json');
+//let verbData = require('./verbs.json');
 let currentConspiracy = ""
 let currentVerb = ""
 let currentNoun = ""
@@ -15,14 +15,35 @@ server.get('/', function(req, res) {
 server.post('/', function(req, res) {
 	//read data
 	console.log(req.body.name)
+
+
 	noun = req.body;
+
+
+
+	if (noun == ""){
+	req.body.name = conspiracyData.nouns[0]
+
+	noun = req.body
+	}
+
+//	noun = req.body;
+
+	console.log(noun.name);
+
 
 	//Here we can code what needs to go to the conspiracy page
 	// -Code-
-	
+	console.log('Conspiracies: ', conspiracyData.conspiracy)
+	console.log('nouns: ', conspiracyData.nouns[0])
+
+
+
+
+
 
 	//reder conspiracy partial
-	res.render('conspiracy', noun)
+	res.render('conspiracy', {noun})
 })
 
 
