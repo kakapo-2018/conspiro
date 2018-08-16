e3da76671dbb4e41b4b38cc6d952d9017896d5ce
student <llukechadwick@gmail.com>
2018-08-16 19:47:03 +1200

    get basic functionality working

--- routes.js
+++ routes.js
@@ -1,10 +1,28 @@
 let express = require('express');
 let	server = express();
-// let data = require('./data.json');
-    
+let conspiracyData = require('./conspiracy.json');
+let nounData = require('./nouns.json');
+let verbData = require('./verbs.json');
+let currentConspiracy = ""
+let currentVerb = ""
+let currentNoun = ""
 
 server.get('/', function(req, res) {
-	res.render('layouts/main')
+	//reder conspiracy partial
+	res.render('index')
+})
+
+server.post('/', function(req, res) {
+	//read data
+	console.log(req.body.name)
+	noun = req.body;
+
+	//Here we can code what needs to go to the conspiracy page
+	// -Code-
+	
+
+	//reder conspiracy partial
+	res.render('conspiracy', noun)
 })
 
 
